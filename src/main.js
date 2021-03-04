2fe42ea9a4ee2fba7e738c29d3dd3dd4fbcad728
Tianjie <1321467235@qq.com>
2021-03-04 17:32:49 +0800

    项目初始化

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/*
+ * @Author: mikey.zhaopeng 
+ * @Date: 2021-03-04 14:13:23 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2021-03-04 14:18:53
+ */
+
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
